52e484f9c66f37263a8fb74b42ade330b71b7194
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-10 22:59:54 +0500

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        for(int i = 65; i<=90; i++) {
+            char d = (char)i;
+            System.out.println(d);
+        }
     }
 }
