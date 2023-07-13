7617d8394578fe8c2cba8c8de7133f4e0298be35
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-13 20:52:04 +0500

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+    int[] arr = new int[lastFibonacci];
+        arr[0] = 0;
+        arr[1] = 1;
+    for(int i=2;i<arr.length;i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+        System.out.println(arr[i]);
+        }
     }
 }
