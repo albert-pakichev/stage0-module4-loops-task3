b25bc4603cf438149f48442e6bee3e0d71260441
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-15 11:04:51 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,14 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
-
+    public void printDigitsSum(int number){
+        int sum = 0;
+        char nums[] = new char[Integer.toString(number).length()];
+        for (int i = 0; i < Integer.toString(number).length(); i++)
+        {
+            nums[i] = Integer.toString(number).charAt(i);
+            sum = sum + (nums[i] - '0');
+        }
+        System.out.println(sum);
     }
 }
