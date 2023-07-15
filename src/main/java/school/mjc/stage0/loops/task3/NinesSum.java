fa5fc6c972dabf987c67169ed4012a216201b2fb
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-15 11:17:09 +0500

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,13 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-        int p = 9;
+       int p = 9;
         int sum = 0;
-        for(int i=1;i<=lengthOfLastNumber;i++) {
-            sum+=p;
-            p=(p*10)+9;
+        if (lengthOfLastNumber > 0) {
+            for (int i = 1; i <= lengthOfLastNumber; i++) {
+                sum += p;
+                p = (p * 10) + 9;
+            }
+        } else {
+            System.out.println("Write new number");
         }
         System.out.print(sum);
-
     }
 }
